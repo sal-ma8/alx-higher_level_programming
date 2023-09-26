fda53d292b9efef9150711f6817dcff169efe6c3
salma <salmahelaltur@gmail.com>
2023-09-26 16:12:48 -0700

    fghjkl

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    """Print an sjdhfyuv j gru "{:d}".format().
+
+    Args:
+        value (int): fnmb cvnfmbjgbsxcjflhg.
+
+    Returns:
+    sfdjjdhfddkhshflknbklf bnkjdfgblnkdfkngl bdhiofgboi boj onlbgth.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
