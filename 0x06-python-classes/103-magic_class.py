36ec688ad06120247a86a65c74087fc07d6e5158
salma <salmahelaltur@gmail.com>
2023-09-26 15:50:06 -0700

    bsvnm

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Define a MagicClass matching exactly ."""
+
+import math
+
+
+class MagicClass:
+    """Represent a circle."""
+
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+
+        Arg:
+            radius : this is a function which doing som opration for our program..
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """this is a function which doing som opration for our program.."""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """this is a function which doing som opration for our program."""
+        return (2 * math.pi * self.__radius)
