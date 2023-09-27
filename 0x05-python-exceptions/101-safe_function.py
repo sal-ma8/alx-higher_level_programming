73ef658b188e87976f3d43922ede0ea1d8feac0c
salma <salmahelaltur@gmail.com>
2023-09-26 18:14:02 -0700

    sdfgh

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    """DGYFUDGH JDFLJH HIIR HDFJK.
+
+    Args:
+        fct: DHFYUEDU YR7Y GASODO EIZ.
+        args: GDFYUGVRIB GBUVR .
+
+    Returns:
+        shfuhi huidhfuui hr uhe euiy uehfigcsdG AJFG RH.
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
+
