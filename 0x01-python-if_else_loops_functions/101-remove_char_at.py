05b3318ca32dff26a6cf85ff56f30eb6f93d5b8a
salma <salmahelaltur@gmail.com>
2023-09-26 18:36:21 -0700

    dacgfhvbn

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 101-remove_char_at.py
+
+
+def remove_char_at(str, n):
+    """dfhiuendcfoe jiyg rihnh iofcujghru ri8qri fhf."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
