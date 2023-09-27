d49858eb0458b8a79154d80d64263e847a6cb3a2
salma <salmahelaltur@gmail.com>
2023-09-27 12:18:40 -0700

    dscjbv

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """the function def add(a, b): from the file add_0.py 
-    and prints the result of the addition."""
     from add_0 import add
-
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
