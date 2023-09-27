cd79d4544cc5a43871d34032f77cdc279574f364
salma <salmahelaltur@gmail.com>
2023-09-27 14:11:21 -0700

    gcdj

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """
+    dkfhirnhtgnirlk
+    fghidbhs hfgjh kgkbk;jhlnghkf
+    egfuyvrgbfklhb
+     hfkghfhghhgl hjkljhkjkhjgjv h
+    """
+
+    if idx >= 0 and idx <= (len(my_list) - 1):
+        my_list[idx] = element
+    return my_list
