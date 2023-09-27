8ee70c049327fd4515beb0503267f725ccadc4e5
salma <salmahelaltur@gmail.com>
2023-09-27 14:05:44 -0700

    gcdj

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    jksfjksgbl
+    fvjkdfbkdfvklf
+    dkfnbvln
+    fvkdnbngfbgb;lkp
+    """
+
+    for element in my_list:
+        print("{:d}".format(element))
