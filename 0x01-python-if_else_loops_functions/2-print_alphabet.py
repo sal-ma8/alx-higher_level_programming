0e595208a361744213d31260e0c0f2c3643f7b1c
salma <salmahelaltur@gmail.com>
2023-09-26 18:32:12 -0700

    hdbln

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
 
-"""Print the alphabet in lowercase, not followed by a new line."""
+"""sdbfj uue92 i jdbjhfj."""
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
