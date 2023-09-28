40d5b18706d20d1b407878b568c9a8fd4c3a0e57
salma <salmahelaltur@gmail.com>
2023-09-28 09:17:18 -0700

    bdb

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """
+    djflngind
+    ndipnsiufnpo
+    nbinsopwn
+    kksinfol
+    """
+
+    return ("".join([c for c in my_string if c not in ['c', 'C']]))
