9c5348a0c61970634b414f4a59555d30492c7d0b
salma <salmahelaltur@gmail.com>
2023-09-28 09:41:19 -0700

    kjdfb

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """
+    ghfkbelw df
+    jfuenmd;w
+    bfinlgo
+    """
+
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
