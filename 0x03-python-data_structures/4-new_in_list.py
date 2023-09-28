1b3754550d36d00f4b0ffca0faab0847d07a6020
salma <salmahelaltur@gmail.com>
2023-09-28 09:15:42 -0700

    vdjhsbf

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """
+    fgjvknjfi rivnpofek
+    rgji rhjrjkg;l;gld; ouj j
+    jfkdljgkf
+    """
+
+    list_copy = my_list.copy()
+
+    if idx >= 0 and idx <= (len(my_list) - 1):
+        list_copy[idx] = element
+    return list_copy
