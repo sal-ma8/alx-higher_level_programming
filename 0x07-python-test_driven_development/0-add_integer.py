82bf0ba91c0a57d1a06583e196a02a4510dfe5dc
salma <salmahelaltur@gmail.com>
2023-10-23 13:00:29 -0700

    ghjk

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this is the first file for this project.
+this file function to add two number tohather.
+>>> add_integer(10, 12)
+"""
+
+
+def add_integer(a, b=98):
+    """
+        Adds two integer.
+        Args:
+            a (int|float): a is number int or float.
+            b (int|float): b is number int or float.
+        Returns: The sum of `a` and `b`
+    """
+
+    if type(a) is float:
+        a = int(a)
+
+    if type(b) is float:
+        b = int(b)
+
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return (a + b)
