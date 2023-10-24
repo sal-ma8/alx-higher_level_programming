7da7731beedaa8d82f0e60cc10698f9a90faee67
salma <salmahelaltur@gmail.com>
2023-10-24 10:54:05 -0700

    dhjvfhk

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This file ia last before in this project.
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    multiply 2 matrix that is given
+    Args:
+        m_a: frist input matrix
+        m_b: second input matrix
+    Returns:
+        return m2 * m2
+    """
+    return numpy.matmul(m_a, m_b)
