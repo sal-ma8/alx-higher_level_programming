90e567494ed75e84611ad5f49e715d591253b69f
salma <salmahelaltur@gmail.com>
2023-10-28 23:55:57 -0700

    regfr

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ragrtethhhw trhyerj yey
+"""
+
+
+class MyInt(int):
+    """dfdgf fgsdhh thrh th ts"""
+    def __new__(cls, *args, **kwargs):
+        """etrhetrhyt yteh yethn jwye """
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """twrher trheteyrhyhteyhyet """
+        return int(self) != other
+
+    def __ne__(self, other):
+        """rertwyhe tth yt getgthetr heththh """
+        return int(self) == other
