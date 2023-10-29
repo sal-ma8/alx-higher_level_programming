b211b118201f252a1822d432dce3f1d4e733b722
salma <salmahelaltur@gmail.com>
2023-10-28 22:43:55 -0700

    sdfbk

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''dfgigiu rifhgsruguia.'''
 
+
 def inherits_from(obj, a_class):
     '''kdjgfug kfhfgifhgfiulg ijblgjd.'''
     return isinstance(obj, a_class) and type(obj) != a_class
