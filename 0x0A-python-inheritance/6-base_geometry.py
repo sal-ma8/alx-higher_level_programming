43d35ce282cdd87666169456db73baf7b24d07c7
salma <salmahelaltur@gmail.com>
2023-10-28 22:47:45 -0700

    rfnkr

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''dfgrihfgihrahv fhgiu wrhighhghighui.'''
+
+
+class BaseGeometry:
+    '''kafhoghfujogh ofhbhfohpofhjvofhdp.'''
+    def area(self):
+        '''rhfoiahg fhaiohgoahfioghiofhvo.'''
+        raise Exception('area() is not implemented')
