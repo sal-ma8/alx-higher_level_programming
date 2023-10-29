9e7bef99da6fa7e711892d5cbf5809be28bb1ff1
salma <salmahelaltur@gmail.com>
2023-10-28 22:28:18 -0700

    sf

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''THIS IS A FRIST FILE FOR THIS PROLECT.'''
+
+
+def lookup(obj):
+    '''ADKFJAFASB;FKJFJDKVC VLN DNA.
+    Args:
+        obj (object): kdnslknfld
+
+    Returns:
+        list: some object in list
+    '''
+    return dir(obj)
