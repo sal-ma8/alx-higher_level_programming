4e821168ca9b443c3b3b40e018ea8a79b7e2da86
salma <salmahelaltur@gmail.com>
2023-10-30 12:34:36 +0000

    f

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ehriohf hflkgjjd fi ah
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    to_json_string - fkjij b vj jfvldk nv bg(string):
+    Args:
+        my_obj: fvfovbnfjop orfkp jofih vh 
+    Return: zvhonied epjhvif opvp p
+    """
+    return json.dumps(my_obj)
