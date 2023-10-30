2cd198411aa13337145880140bfb7eaba78ee288
salma <salmahelaltur@gmail.com>
2023-10-30 14:58:31 -0700

    fdgk

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+hdhgjprjnbgijhflkgb;fjtpd fibnuga fjfk 
+"""
+
+
+def class_to_json(obj):
+    """
+    fjgjfl;z'kgjgj rjgzp jkgjfzjls mvshvfdgvdfkhfkhkhgkb hfjkgh fgra.
+    """
+    return obj.__dict__
