50b3a3a682caa3e1cbb6afa9356351ba3e9999c7
salma <salmahelaltur@gmail.com>
2023-10-30 12:49:46 +0000

    aef

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+dnfldho rhiohhg uggigyfg;
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    save_to_json_file -  fugjok fk[wqk rivjhvv ojprf fdbetqggbd:
+    Args:
+        my_obj: gfdssdn sfhnsfm
+        filename: sfghsfsgsh fgn
+    Return: fgtht fhjftnffhmg
+    """
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
