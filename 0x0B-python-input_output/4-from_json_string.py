dfbd42aeb5daa27d0b580d659e12dbde623b8945
salma <salmahelaltur@gmail.com>
2023-10-30 12:46:06 +0000

    df

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+4-from_json_string module
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    from_json_string - bkzjritjriogo jrhruof jlhiogjrigo; fjgiou[5i pogahugiuvjd:
+
+      Args:
+        my_str: FTFG G YGHO V U FYGUSRD HUIYUG GIUTI
+
+    Return: object
+    """
+    return json.loads(my_str)
